ab969db66c466c1ee8186ec94bc87d4afa96a43f
thomasjames29-dotcom <thomasjames29@live.co.uk>
2026-01-31 19:00:39 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,30 @@
-const CACHE_NAME = 'acca-master-v1';
+const CACHE_NAME = 'acca-master-v2-live'; // Changed to force update
 const ASSETS = [
   './index.html',
   './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Forces new worker to take over immediately
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  // Delete old caches (v1) to remove fake data
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+            return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
